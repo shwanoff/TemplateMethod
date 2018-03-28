10873b0708c7d2c1ce94dfc75296f70f0c627c17
Вадим Шванов <adm.shwan@gmail.com>
2018-03-28 18:40:16 +0300

    Add project files.

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            // Создаем два вида пирога.
+            var meatPie = new MeatPie();
+            var applePie = new ApplePie();
+
+            // Готовим мясной пирог.
+            Console.WriteLine(meatPie);
+            meatPie.Cook();
+            Console.ReadLine();
+
+            // Готовим яблочный пирог.
+            Console.WriteLine(applePie);
+            applePie.Cook();
+            Console.ReadLine();
+        }
+    }
+}
